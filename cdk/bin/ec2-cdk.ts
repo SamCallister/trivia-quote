e965912d1ea4dae7cd8b573fa84837029201a68c
Sam Callister <samc@MacBook-Pro.local>
2022-09-19 21:12:02 -0700

    cdk simple load balancer architecture

--- cdk/bin/ec2-cdk.ts
+++ cdk/bin/ec2-cdk.ts
@@ -1,6 +1,12 @@
 import * as cdk from 'aws-cdk-lib';
 import { Ec2CdkStack } from '../lib/ec2-cdk-stack';
 
+
+const envUSA = { account: '055588682254', region: 'us-west-2' };
+
 const app = new cdk.App();
 
-new Ec2CdkStack(app, 'Ec2CdkStack', {description: "Public facing ec2 instance running node app"});
+new Ec2CdkStack(app, 'LoadBalancerTriviaQuote', {
+	description: "Public facing ec2 instance running node app",
+	env: envUSA
+});
--- cdk/lib/ec2-cdk-stack.ts
+++ cdk/lib/ec2-cdk-stack.ts
@@ -1,6 +1,10 @@
 import * as ec2 from "aws-cdk-lib/aws-ec2";
 import * as cdk from 'aws-cdk-lib';
-import * as iam from 'aws-cdk-lib/aws-iam'
+import * as route53 from 'aws-cdk-lib/aws-route53';
+import * as targets from 'aws-cdk-lib/aws-route53-targets';
+import * as autoscaling from 'aws-cdk-lib/aws-autoscaling';
+import * as elbv2 from 'aws-cdk-lib/aws-elasticloadbalancingv2';
+import * as acm from 'aws-cdk-lib/aws-certificatemanager';
 import * as path from 'path';
 // import { KeyPair } from 'cdk-ec2-key-pair';
 import { Asset } from 'aws-cdk-lib/aws-s3-assets';
@@ -25,72 +29,85 @@ export class Ec2CdkStack extends cdk.Stack {
 				cidrMask: 24,
 				name: "asterisk",
 				subnetType: ec2.SubnetType.PUBLIC
-			}]
+			}],
 		});
 
-		// Allow SSH (TCP Port 22) access from anywhere
-		const securityGroup = new ec2.SecurityGroup(this, 'SecurityGroup', {
+		const asg = new autoscaling.AutoScalingGroup(this, 'ASG', {
 			vpc,
-			description: 'Allow SSH (TCP port 22) in',
-			allowAllOutbound: true
-		});
-		securityGroup.addIngressRule(ec2.Peer.anyIpv4(), ec2.Port.tcp(22), 'Allow SSH Access');
-		securityGroup.addIngressRule(ec2.Peer.anyIpv4(), ec2.Port.tcp(80), 'Allow http access');
-		securityGroup.addIngressRule(ec2.Peer.anyIpv4(), ec2.Port.tcp(443), 'Allow https access');
-
-		const role = new iam.Role(this, 'ec2Role', {
-			assumedBy: new iam.ServicePrincipal('ec2.amazonaws.com')
+			instanceType: ec2.InstanceType.of(ec2.InstanceClass.T3, ec2.InstanceSize.NANO),
+			machineImage: new ec2.AmazonLinuxImage(),
+			desiredCapacity: 1,
+			minCapacity: 1,
+			maxCapacity: 1
 		});
 
-		role.addManagedPolicy(iam.ManagedPolicy.fromAwsManagedPolicyName('AmazonSSMManagedInstanceCore'));
 
-		// Use Latest Amazon Linux Image - CPU Type ARM64
-		const ami = new ec2.AmazonLinuxImage({
-			generation: ec2.AmazonLinuxGeneration.AMAZON_LINUX_2,
-			cpuType: ec2.AmazonLinuxCpuType.ARM_64
-		});
 
-		// Create the instance using the Security Group, AMI, and KeyPair defined in the VPC created
-		const ec2Instance = new ec2.Instance(this, 'Instance', {
+		const lb = new elbv2.ApplicationLoadBalancer(this, 'LB', {
 			vpc,
-			instanceType: ec2.InstanceType.of(ec2.InstanceClass.T4G, ec2.InstanceSize.NANO),
-			machineImage: ami,
-			securityGroup: securityGroup,
-			// keyName: key.keyPairName,
-			role: role
+			internetFacing: true,
 		});
+
+		const MY_DOMAIN = "triviaquote.com";
+		const zone = route53.HostedZone.fromLookup(this, 'Zone', { domainName: MY_DOMAIN })
+
+
+		const certificate = new acm.Certificate(this, 'Certificate', {
+			domainName: MY_DOMAIN,
+			validation: acm.CertificateValidation.fromDns(zone),
+		});
+
+		const target = route53.RecordTarget.fromAlias(new targets.LoadBalancerTarget(lb));
+
+		new route53.ARecord(this, "triviaQuoteARecord", {
+			zone,
+			target,
+		});
+
+		const listener = lb.addListener("listener", {
+			certificates: [elbv2.ListenerCertificate.fromArn(certificate.certificateArn)],
+			port: 443,
+			protocol: elbv2.ApplicationProtocol.HTTPS
+		});
+
+		listener.addTargets('Target', {
+			port: 80,
+			targets: [asg]
+		});
+
+		listener.connections.allowDefaultPortFromAnyIpv4('Open to the world');
 
 		// Create an asset that will be used as part of User Data to run on first load
 		const asset = new Asset(this, 'codeZip', { path: path.join(__dirname, '../../dist.zip') });
 
-
 		const userDataScript = readFileSync(
 			path.join(__dirname, '../src/config.sh')
 			, 'utf8');
-		ec2Instance.addUserData(userDataScript);
-		const localPath = ec2Instance.userData.addS3DownloadCommand({
+
+		asg.addUserData(userDataScript)
+		const localPath = asg.userData.addS3DownloadCommand({
 			bucket: asset.bucket,
 			bucketKey: asset.s3ObjectKey,
 		});
 
-		// ec2Instance.addUserData();
-		ec2Instance.userData.addCommands(
+		asg.userData.addCommands(
 			`mkdir server`,
 			`cd server`,
 			`unzip ${localPath}`,
 			`npm install`,
 			`npm run start:prod`
-		);
-		ec2Instance.userData.addExecuteFileCommand({
+		)
+
+		asg.userData.addExecuteFileCommand({
 			filePath: localPath,
 			arguments: '--verbose -y'
-		});
-		asset.grantRead(ec2Instance.role);
+		})
 
-		// Create outputs for connecting
-		new cdk.CfnOutput(this, 'IP Address', { value: ec2Instance.instancePublicIp });
-		// new cdk.CfnOutput(this, 'Key Name', { value: key.keyPairName })
-		new cdk.CfnOutput(this, 'Download Key Command', { value: 'aws secretsmanager get-secret-value --secret-id ec2-ssh-key/cdk-keypair/private --query SecretString --output text > cdk-key.pem && chmod 400 cdk-key.pem' });
-		new cdk.CfnOutput(this, 'ssh command', { value: 'ssh -i cdk-key.pem -o IdentitiesOnly=yes ec2-user@' + ec2Instance.instancePublicIp });
+		asg.scaleOnRequestCount('AModestLoad', {
+			targetRequestsPerMinute: 10000,
+		});
+
+		asset.grantRead(asg.role);
+
 	}
 }
